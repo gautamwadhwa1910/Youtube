2f319cde246b75dc1172a0a54c3bc715d2637b6d
gautamwadhwa1910 <bussgautam07@gmail.com>
2026-03-03 10:25:03 +0530

    add structured changes

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const { APP_NAME } = require('./constant');
+
+function start() {
+  console.log(`${APP_NAME} started`);
+}
+
+module.exports = { start };
---
+++ src/constant.js
@@ -0,0 +1,4 @@
+module.exports = {
+  APP_NAME: 'youtube-app',
+  PORT: process.env.PORT || 3000,
+};
